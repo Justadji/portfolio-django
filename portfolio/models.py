f5e4de218f1f271c8057ffcf7d73f000fe7769be
njab <justenganongo@gmail.com>
2025-07-29 22:45:08 +0100

    Superuser

--- portfolio/models.py
+++ portfolio/models.py
@@ -32,7 +32,7 @@ STYLES = [
 
 class Commande(models.Model):
     nom = models.CharField(max_length=100)
-    prenom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=200)
     email = models.EmailField()
     format = models.CharField(max_length=10, choices=FORMATS, default="A4")
     style = models.CharField(max_length=20, choices=STYLES, default="Portrait")
